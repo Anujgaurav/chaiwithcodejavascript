00581352a33070beca7fd5f5900907ffe86debcb
ANUJ GAURAV <98847669+Anujgaurav@users.noreply.github.com>
2023-09-11 11:58:47 +0000

    other type conversion

--- dataconversion.js
+++ dataconversion.js
@@ -1,4 +1,9 @@
-let score = "34";
+// let score = "34";
+// let score = "anuj"
+// let score = true;
+// let score = false;
+let score = "33abd";
+
 
 // console.log(typeof(score));
 let valueinnumber = Number(score);
