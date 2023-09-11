ea0a9c01aed25dfc319b2914eb243cc31008d650
ANUJ GAURAV <98847669+Anujgaurav@users.noreply.github.com>
2023-09-11 11:23:38 +0000

    agian

---
+++ let_const.js
@@ -0,0 +1,8 @@
+const customer_id = 1233232;
+let customer_name = "anuj gaurav";
+
+ customer_city="delhi";
+
+  customer_name= "Riya Tomar";
+
+ console.table([customer_id,customer_name,customer_city]);
