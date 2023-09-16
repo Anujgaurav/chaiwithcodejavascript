fcd6946f54af3cc49a5d8db602131635609e1bdc
ANUJ GAURAV <98847669+Anujgaurav@users.noreply.github.com>
2023-09-16 09:21:28 +0000

    number

---
+++ nums.js
@@ -0,0 +1,13 @@
+const score= 400;
+
+const balanace= new Number(100);
+console.log(balanace);
+
+console.log(balanace.toString().length);
+console.log(balanace.toFixed(3));
+
+const Other=123.8966
+console.log(Other.toPrecision(4));
+
+const hundrade=1000000
+console.log(hundrade.toLocaleString('en-IN'));
