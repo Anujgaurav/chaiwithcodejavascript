801ff72fcdd1cd562b4c6b6a3d71c551fc7e1daa
ANUJ GAURAV <98847669+Anujgaurav@users.noreply.github.com>
2023-12-25 15:35:44 +0000

    a

---
+++ object.js
@@ -0,0 +1,12 @@
+const anuj = {
+    name : "Anuj Gaurav",
+    "Full Name": "Anuj Gaurav",
+    age : 18,
+    Location: "Moradabad",
+    Email: "Anuj@gmail.com",
+    LastLoginDays:["Monday","Sunday"]
+
+
+}
+console.log(anuj.Email);// try to leave this type of object to accesss the results
+console.log(anuj["Full Name"]);
