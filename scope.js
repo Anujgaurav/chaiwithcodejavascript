afd9fcbddfc59d87beb968441205840f805968c9
ANUJ GAURAV <98847669+Anujgaurav@users.noreply.github.com>
2023-12-28 22:37:17 +0000

    scope

---
+++ scope.js
@@ -0,0 +1,13 @@
+function anuj() {
+    const username = "anuj-gaurav"
+
+    function gaurav( ) {
+        const website = " Chrome"
+        console.log(username + website);
+        
+    }
+    // console.log(website);
+  gaurav()  
+}
+// console.log(username);
+anuj()
