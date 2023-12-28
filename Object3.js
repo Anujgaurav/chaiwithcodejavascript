a8ac9108421d39eaf8af74f31f85e08bdfddc44b
ANUJ GAURAV <98847669+Anujgaurav@users.noreply.github.com>
2023-12-28 18:13:20 +0000

    json learn

---
+++ Object3.js
@@ -0,0 +1,12 @@
+const course={
+   courseName:"English",
+   courseSub:"white",
+   courseTeacher:"Rakesh"
+}
+const{ courseTeacher: anuj}=  course
+// console.log(courseTeacher);
+console.log(anuj);
+
+
+
+// json formate is the tool where we easily undersatnd the api formate
