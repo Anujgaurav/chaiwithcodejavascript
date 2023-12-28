33a6215d8189e7c795009b1e8bc8127b7d3721dc
ANUJ GAURAV <98847669+Anujgaurav@users.noreply.github.com>
2023-12-28 21:42:26 +0000

    functions

---
+++ function1.js
@@ -0,0 +1,32 @@
+// function anuj(a,b) {
+// // console.log("A");    
+// // console.log("B");    
+// // console.log("C");    
+// // console.log("D");    
+// // console.log("E");    
+// // console.log("F");    
+// // console.log("G");
+
+//  console.log(a+b) 
+
+// }
+
+// anuj(4,5)
+
+// anuj(8,5)
+
+function sum(number1, number2) {
+return number1 + number2    
+}
+
+const result = sum(6,5)
+console.log("my result :", result);
+
+// name ya string print krane k liye 
+
+function Login(username){
+    return `${username} how are yu `
+}
+
+console.log(Login("anuj"));
+
