aef36a0b3635a087a0f3517b7b2df4adbe77c8c4
ANUJ GAURAV <98847669+Anujgaurav@users.noreply.github.com>
2023-12-28 14:42:31 +0000

    object 2 finsihed

--- Object2.js
+++ Object2.js
@@ -26,3 +26,20 @@ const Gaurav={
 
 console.log(Gaurav.username.fullname.firstname);
 console.log(Gaurav.username.fullname.Lastname);
+console.log(Gaurav.username.fullname?.Lastname);
+
+
+const ob = {1:"a",2:"b",3:"c"}
+const ob1 = {4:"a",5:"b",6:"c"}
+const ob2= {7:"a",8:"b",9:"c"}
+// const ob3= Object.assign({},ob,ob1,ob2);
+// maximum use hone wala syntax sprit (chord dena) wala he use hota hai 
+const ob3= {...ob,...ob1,...ob2}
+console.log(ob3);
+
+console.log(Object.keys(Gaurav));
+console.log(Object.values(Gaurav));
+console.log(Object.entries(Gaurav));
+
+console.log(Gaurav.hasOwnProperty('emailid'));
+console.log(Gaurav.hasOwnProperty('fullname'));
