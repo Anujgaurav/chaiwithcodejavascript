fa5ed470c8a4a86755938ff2fab1268c3b1afef6
ANUJ GAURAV <98847669+Anujgaurav@users.noreply.github.com>
2023-12-29 10:07:52 +0000

    arrow

---
+++ Arrow_function.js
@@ -0,0 +1,13 @@
+// Syntax ()=>{}
+
+// two type of method 
+// first method
+const chai=(num1, num2)=>{
+    console.log(num1+num2)
+}
+
+chai(4,5)
+//second method
+const anuj=(num1,num2)=> num1+num2
+
+console.log(anuj(2,3));
