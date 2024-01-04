0edd813a9f898109d30130cea6c119f602073ed3
ANUJ GAURAV <98847669+Anujgaurav@users.noreply.github.com>
2024-01-04 19:02:00 +0000

    iife

---
+++ iife.js
@@ -0,0 +1,23 @@
+(function chai() {
+    console.log("Hello Duniya");
+    
+})
+();
+// syntax of iife Vale==>() call-->() ()()
+
+(function and( ) {
+    console.log("haanjiii");
+    
+})
+();
+
+//  try to use the arrow function 
+(()=>{
+    console.log("hello World");
+})
+();
+
+((name)=>{
+    console.log(`my name is ${name}`);
+})
+(`anuj gaurav`)
