d47b9b07129cb6edad4298a0eff8ff7ceeb7ce7c
ANUJ GAURAV <98847669+Anujgaurav@users.noreply.github.com>
2024-01-04 19:22:30 +0000

    call_stack

---
+++ call_stack.js
@@ -0,0 +1,12 @@
+// // // //  javascript excecution context 
+// // // [ code]   --> global [this ] excecution context
+ 
+// // // 1.global excecution context
+// // // 2. function excecution context
+// // // 3. Eval excecution context
+
+// //  mermory creation phase mai sirf memory located hoti h koi code execute nhi hota hai 
+//  execution phase 
+
+
+
